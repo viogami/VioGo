277d374df58618f0ce19dbd08100f65b6d24683d
chenyw <2654613995@qq.com>
2025-05-23 17:08:38 +0800

    fix

--- cron/ai_cron.go
+++ cron/ai_cron.go
@@ -1,10 +1,25 @@
 package cron
 
 import (
+	"log/slog"
+	"time"
+
 	"github.com/viogami/viogo/AI/deepseek"
 )
 
-func clearChatHistory() {
+type AICronTask struct {
+	CronTask
+}
+
+func (c *AICronTask) ClearChatHistory() {
 	s := deepseek.NewDeepSeekService()
 	s.ClearContext()
+
+	now := time.Now()
+	slog.Info("AICronTask:ClearChatHistory执行成功!", "time", now.Format("2006-01-02 15:04:05"))
+
+}
+
+func NewAICronTask() *AICronTask {
+	return &AICronTask{}
 }
--- cron/init.go
+++ cron/init.go
@@ -4,10 +4,14 @@ import (
 	"github.com/robfig/cron/v3"
 )
 
+type CronTask struct {
+}
+
 func init() {
+	aiCron := NewAICronTask()
+
 	c := cron.New(cron.WithSeconds())
-	
-	c.AddFunc("0 */5 * * * *", clearChatHistory) // 每5分钟执行一次
+	c.AddFunc("0 */5 * * * *", aiCron.ClearChatHistory) // 每5分钟执行一次
 
 	c.Start()
 	defer c.Stop()
